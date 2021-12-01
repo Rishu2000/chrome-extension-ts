dac9c3f0488c81b06dcb483847c72182fd081620
Sahil Deliwala <sahildeliwala93@gmail.com>
2021-12-01 15:28:48 +0530

    Added code to get the current url.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+export interface AppType {}
+
+const App:React.FC<AppType> = () => {
+
+const [url, setUrl] = useState<string | undefined>('');
+
+useEffect(() => {
+  const queryInfo = {active: true, lastFocusedWindow: true};
+  chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
+    const url = tabs[0].url;
+    setUrl(url);
+  })
+}, [])
+
   return (
     <div className="App">
-      Rishav
+      Rishav Kumar <br/>
+      {url}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
